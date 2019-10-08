e2bfaaf1472e21a54cd0085e032d14b955d0957a
cemvarol <cem.varol@outlook.com>
2019-10-08 09:19:48 +0100

    Add files via upload

---
+++ Host1.ps1
@@ -0,0 +1,10 @@
+﻿Set-NetFirewallProfile -Enabled False
+
+Install-WindowsFeature Routing, RSAT-RemoteAccess, Hyper-V-Tools, Hyper-V-PowerShell
+
+New-VMSwitch -SwitchName VMs -SwitchType Internal
+
+Netsh int ip set address "vEthernet (VMs)" static 1.1.1.1 255.0.0.0
+
+rrasmgmt.msc
+
---
+++ Host2.ps1
@@ -0,0 +1,14 @@
+﻿netsh routing ip nat install
+
+netsh routing ip nat add interface "Ethernet 2" mode=full
+
+netsh routing ip nat add interface "vEthernet (VMs)"
+
+cd downloads
+
+ren .\9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd 2012R2.vhd
+
+New-VM -VHDPath .\2012R2.vhd -Confirm -Generation 1 -MemoryStartupBytes 4GB -Name 2012-Re2 -Path c:\VMs\ -Switch VMs –Force
+
+
+ virtmgmt.msc

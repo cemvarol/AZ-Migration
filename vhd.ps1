c7c3861d029eec9e12ab026ed1744d90444f9739
cemvarol <cem.varol@outlook.com>
2020-03-31 21:32:52 +0100

    Update vhd.ps1

--- vhd.ps1
+++ vhd.ps1
@@ -1,4 +1,9 @@
-$url = "http://download.microsoft.com/download/5/8/1/58147EF7-5E3C-4107-B7FE-F296B05F435F/9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd"
-$output = "$env:USERPROFILE\downloads\9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd"
+$url = "https://github.com/cemvarol/AZ-Migration/blob/master/ChromeSetup.exe?raw=true"
+$output = "$env:USERPROFILE\downloads\ChromeSetup.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" 
+
+& "$env:USERPROFILE\downloads\ChromeSetup.exe"
+
+Start-Sleep -s 30
+
+& "C:\Program Files (x86)\google\chrome\Application\chrome.exe" http://download.microsoft.com/download/5/8/1/58147EF7-5E3C-4107-B7FE-F296B05F435F/9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd

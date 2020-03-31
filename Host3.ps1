593161e44af65346c1219acbd55b883eee2d9947
cemvarol <cem.varol@outlook.com>
2020-03-31 18:31:51 +0100

    Delete Host3.ps1

--- Host3.ps1
+++
@@ -1,8 +0,0 @@
-﻿cd $env:USERPROFILE\downloads
-
-ren .\9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd 2012-R2.vhd
-
-New-VM -VHDPath .\2012-R2.vhd -Confirm -Generation 1 -MemoryStartupBytes 4GB -Name 2012-R2 -Path c:\VMs\ -Switch VMs –Force
-
-
- virtmgmt.msc

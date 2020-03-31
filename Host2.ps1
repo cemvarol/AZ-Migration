7c3bdaca832afee0ff17de04afe235e1090c2241
cemvarol <cem.varol@outlook.com>
2020-03-31 18:30:59 +0100

    Update Host2.ps1

--- Host2.ps1
+++ Host2.ps1
@@ -4,11 +4,11 @@ netsh routing ip nat add interface "Ethernet 2" mode=full
 
 netsh routing ip nat add interface "vEthernet (VMs)"
 
-cd downloads
+cd $env:USERPROFILE\downloads
 
-ren .\9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd 2012R2.vhd
+ren .\9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd 2012-R2.vhd
 
-New-VM -VHDPath .\2012R2.vhd -Confirm -Generation 1 -MemoryStartupBytes 4GB -Name 2012-Re2 -Path c:\VMs\ -Switch VMs –Force
+New-VM -VHDPath .\2012-R2.vhd -Confirm -Generation 1 -MemoryStartupBytes 4GB -Name 2012-R2 -Path c:\VMs\ -Switch VMs –Force
 
 
  virtmgmt.msc

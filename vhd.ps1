ad396cfdde78f865960fed5ba9182ce4eea69ea7
cemvarol <cem.varol@outlook.com>
2020-03-31 20:38:01 +0100

    Create vhd.ps1

---
+++ vhd.ps1
@@ -0,0 +1,4 @@
+$url = "http://download.microsoft.com/download/5/8/1/58147EF7-5E3C-4107-B7FE-F296B05F435F/9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd"
+$output = "$env:USERPROFILE\downloads\9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd"
+Invoke-WebRequest -Uri $url -OutFile $output
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" 

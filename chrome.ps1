5ab2428e09639b77b182a33485b4c7c6b471393e
cemvarol <cem.varol@outlook.com>
2020-03-31 21:01:42 +0100

    Update chrome.ps1

--- chrome.ps1
+++ chrome.ps1
@@ -1,7 +1,5 @@
 $url = "https://github.com/cemvarol/AZ-Migration/blob/master/ChromeSetup.exe?raw=true"
 $output = "$env:USERPROFILE\downloads\ChromeSetup.exe"
-$start_time = Get-Date
 Invoke-WebRequest -Uri $url -OutFile $output
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" 
 
 & "$env:USERPROFILE\downloads\ChromeSetup.exe"

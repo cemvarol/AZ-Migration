1eb23d9a1d249eaa827a554eb46517771e116714
cemvarol <cem.varol@outlook.com>
2020-04-15 07:38:32 +0100

    Update Host2.ps1

--- Host2.ps1
+++ Host2.ps1
@@ -24,7 +24,7 @@ copy .\Guest.ps1 f:\
 
 Dismount-VHD –Path ".\2012-R2.vhd"
 
-New-VM -VHDPath .\2012-R2.vhd -Confirm -Generation 1 -MemoryStartupBytes 3GB -Name 2012-R2 -Path c:\VMs\ -Switch VMs –Force
+New-VM -VHDPath .\2012-R2.vhd -Confirm -Generation 1 -MemoryStartupBytes 4GB -Name 2012-R2 -Path c:\VMs\ -Switch VMs –Force
 
 $url = "https://raw.githubusercontent.com/cemvarol/AZ-Migration/master/Guest.ps1"
 $output = ".\Guest.ps1"

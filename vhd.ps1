35be4fc9ad8254ce42cba8605ab0eceeccbc40da
cemvarol <cem.varol@outlook.com>
2020-06-02 07:56:31 +0100

    Update vhd.ps1

--- vhd.ps1
+++ vhd.ps1
@@ -4,6 +4,6 @@ Invoke-WebRequest -Uri $url -OutFile $output
 
 & "$env:USERPROFILE\downloads\ChromeSetup.exe"
 
-Start-Sleep -s 30
+Start-Sleep -s 60
 
 & "C:\Program Files (x86)\google\chrome\Application\chrome.exe" http://download.microsoft.com/download/5/8/1/58147EF7-5E3C-4107-B7FE-F296B05F435F/9600.16415.amd64fre.winblue_refresh.130928-2229_server_serverdatacentereval_en-us.vhd
